0f747b4f37e875bc74aa86a0ca3ac47ca355f0ff
Simona Landauskienė <97624622+SimonaMarcinkeviciute@users.noreply.github.com>
2022-10-26 15:02:25 +0300

    Delete Spring/eshopapi/src/main directory

--- Spring/eshopapi/src/main/java/lt/codeacademy/eshopapi/exception/FileException.java
+++
@@ -1,7 +0,0 @@
-package lt.codeacademy.eshopapi.exception;
-
-public class FileException extends RuntimeException {
-    public FileException(String message) {
-        super(message);
-    }
-}
--- Spring/eshopapi/src/main/java/lt/codeacademy/eshopapi/security/Login.java
+++
@@ -1,4 +0,0 @@
-package lt.codeacademy.eshopapi.security;
-
-public record Login(String username, String password) {
-}
